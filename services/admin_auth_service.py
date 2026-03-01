7492210e2da71662df13655698acffe188956625
thirat <thirat.mi.68@ubu.ac.th>
2026-03-02 03:32:07 +0700

    feat: Implement staff/admin authentication and introduce dedicated base templates for user and staff interfaces.

--- services/admin_auth_service.py
+++ services/admin_auth_service.py
@@ -1,7 +1,25 @@
 from models import db, Admin
+from werkzeug.security import check_password_hash, generate_password_hash
 
-def verify_admin_login(employee_id, user_name, password):
-    pass
+# API Logic
+def verify_admin_login(login_id, password):
+    admin = Admin.query.filter(
+        (Admin.Employee_id == login_id) | (Admin.User_name == login_id)
+    ).first()
+    
+    if admin and check_password_hash(admin.hash_password, password):
+        return admin
+    return None
 
 def create_admin_staff(first_name, last_name, employee_id, user_name, password):
-    pass
+    hashed_pw = generate_password_hash(password)
+    new_admin = Admin(
+        first_name=first_name,
+        last_name=last_name,
+        Employee_id=employee_id,
+        User_name=user_name,
+        hash_password=hashed_pw
+    )
+    db.session.add(new_admin)
+    db.session.commit()
+    return new_admin
